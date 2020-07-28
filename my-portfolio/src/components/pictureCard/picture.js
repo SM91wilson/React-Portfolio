953c33a588aa81d749ba7ed51774f2ebb0ae0a4d
tango <sm91wilson@gmail.com>
2020-07-28 15:02:23 -0400

    picture card

---
+++ my-portfolio/src/components/pictureCard/picture.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Picture extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src='' />
+            </div>
+        )
+    }
+}
